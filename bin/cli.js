cfba4eb96402296b39580ee4d53cfea076d9c0b3
Amit Patra <AmitPatra@selectcarleasing.co.uk>
2025-10-08 13:23:33 +0530

    Update version to 1.3.6 - fix CLI version mismatch

--- bin/cli.js
+++ bin/cli.js
@@ -23,7 +23,7 @@ const log = {
 program
   .name('self-serve-review')
   .description('Universal code review tool for microservices')
-  .version('1.3.4');
+  .version('1.3.6');
 
 // Initialize command
 program
